9710b21dc35af83fb5ca9e7c5a9d9b1c7e653497
Antonin Hildebrand <antonin@hildebrand.cz>
2015-01-28 20:07:53 +0100

    Updated test file

--- src/cljs_devtools_sample/core.cljs
+++ src/cljs_devtools_sample/core.cljs
@@ -7,10 +7,6 @@
 
 (println "cljs-devtools-sample")
 
-(.log js/console "****** enable ******")
-
-(dev/support-devtools!)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; some quick and dirty inline tests
@@ -18,19 +14,35 @@
 (def test-atom
   (atom {:number 0
          :string "sample string"
-         :keyword :key
-         :symbol 'sym
+         :keyword :keyword
+         :symbol 'symbol
+         :vector [0,1,2]
          :set '#{a b c}
          :map '{k1 v1 k2 v2}
          :form '[(defn greet [name] (str "Hello, " name "!"))]
          :lambda #(println %)}))
 
 (def test-number 42)
-(def test-vector [0,1,2,3,:end])
+(def test-keyword :keyword)
+(def test-symbol 'symbol)
+(def test-vector [nil 0 "text" [1 2 3 [10 20 30 [100 200 300]]] '#{a b c} {:k1 'v1 :k2 "v2"} #js {"k1" "v1" "k2" :v2} (js* "function(x) { console.log(x); }")])
+(def test-long-vector (range 100))
 
-(.log js/console test-number)
-(.log js/console test-vector)
-(.log js/console test-atom)
+(defn excercise! []
+  (.log js/console test-number)
+  (.log js/console test-keyword)
+  (.log js/console test-symbol)
+  (.log js/console test-vector)
+  (.log js/console test-long-vector)
+  (.log js/console test-atom))
+
+(.log js/console "****** enable ******")
+
+(.log js/console (dev/build-header test-vector))
+
+(dev/support-devtools!)
+
+(excercise!)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
@@ -40,7 +52,4 @@
 
 (dev/unsupport-devtools!)
 
-(.log js/console test-number)
-(.log js/console test-vector)
-(.log js/console test-atom)
-
+(excercise!)
