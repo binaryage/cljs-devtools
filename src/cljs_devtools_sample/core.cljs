7cc0fb082ca3541d0120a56f36c375b56e29200b
Antonin Hildebrand <antonin@hildebrand.cz>
2015-01-29 16:41:00 +0100

    Update playground

--- src/cljs_devtools_sample/core.cljs
+++ src/cljs_devtools_sample/core.cljs
@@ -5,10 +5,7 @@
 (repl/connect "http://localhost:9000/repl")
 (enable-console-print!)
 
-(println "cljs-devtools-sample")
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
 ; some quick and dirty inline tests
 
 (def test-atom
@@ -25,8 +22,10 @@
 (def test-number 42)
 (def test-keyword :keyword)
 (def test-symbol 'symbol)
-(def test-vector [nil 0 "text" [1 2 3 [10 20 30 [100 200 300]]] '#{a b c} {:k1 'v1 :k2 "v2"} #js {"k1" "v1" "k2" :v2} (js* "function(x) { console.log(x); }")])
+(def test-vector [nil 0 0.5 "text" [1 2 3 [10 20 30 [100 200 300]]] '#{a b c} {:k1 'v1 :k2 "v2"} #js {"k1" "v1" "k2" :v2} (js* "function(x) { console.log(x); }")])
 (def test-long-vector (range 100))
+(def test-large-map {:k1 'v1 :k2 'v2 :k3 'v3 :k4 'v4 :k5 'v5 :k6 'v6 :k7 'v7 :k8 'v8 :k9 'v9})
+(def test-interleaved #js {"js" true "nested" {:js false :nested #js {"js2" true "nested2" {:js2 false}}}})
 
 (defn excercise! []
   (.log js/console test-number)
@@ -34,18 +33,21 @@
   (.log js/console test-symbol)
   (.log js/console test-vector)
   (.log js/console test-long-vector)
+  (.log js/console test-large-map)
+  (.log js/console test-interleaved)
+  (.log js/console (.-nested test-interleaved))
   (.log js/console test-atom))
 
-(.log js/console "****** enable ******")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; enable support
 
-(.log js/console (dev/build-header test-vector))
+(.log js/console "****** enable ******")
 
 (dev/support-devtools!)
 
 (excercise!)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
 ; disable support
 
 (.log js/console "****** disable ******")
@@ -53,3 +55,11 @@
 (dev/unsupport-devtools!)
 
 (excercise!)
+
+; ad-hoc debugging
+
+(.log js/console "****** enable ******")
+
+(dev/support-devtools!)
+
+(.log js/console (dev/build-header test-vector))
