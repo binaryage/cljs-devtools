4125b192c8b7a705654d06bb7a3fa61d3818da86
Antonin Hildebrand <antonin@hildebrand.cz>
2015-01-30 23:58:06 +0100

    Update playground

--- src/cljs_devtools_sample/core.cljs
+++ src/cljs_devtools_sample/core.cljs
@@ -6,6 +6,7 @@
 (repl/connect "http://localhost:9000/repl")
 (enable-console-print!)
 (debug/init!)
+(dev/install-devtools!)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; some quick and dirty inline tests
@@ -14,7 +15,8 @@
 (def test-keyword :keyword)
 (def test-symbol 'symbol)
 (def test-vector [nil 0 0.5 "text" [1 2 3 [10 20 30 [100 200 300]]] '#{a b c} {:k1 'v1 :k2 "v2"}])
-(def test-problematic-vector [nil 0 0.5 "text" [1 2 3 [10 20 30 [100 200 300]]] '#{a b c} {:k1 'v1 :k2 "v2"} #js {"k1" "v1" "k2" :v2} (js* "function(x) { console.log(x); }")])
+(def test-problematic-vector [(aget js/window "sampleArray") #js {"k1" "v1" "k2" :v2} (js* "function(x) { console.log(x); }") #(.log js/console "hello")])
+(def test-problematic-vector2 [js/window])
 (def test-long-vector (range 100))
 (def test-large-map {:k1 'v1 :k2 'v2 :k3 'v3 :k4 'v4 :k5 'v5 :k6 'v6 :k7 'v7 :k8 'v8 :k9 'v9})
 (def test-large-set #{1 2 3 4 5 6 7 8 9 10})
@@ -37,18 +39,15 @@
   (.log js/console test-keyword)
   (.log js/console test-symbol)
   (.log js/console test-vector)
-  (.log js/console test-long-vector)
   (.log js/console test-large-map)
   (.log js/console test-large-set)
-  (.log js/console test-atom))
-;  (.log js/console test-problematic-vector)
-;  (.log js/console test-interleaved)
-;  (.log js/console (.-nested test-interleaved))
+  (.log js/console test-atom)
+  (.log js/console test-problematic-vector)
+  (.log js/console test-problematic-vector2)
+  (.log js/console (.-nested test-interleaved))
+  (.log js/console test-interleaved)
+  )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; enable support
-
-(dev/install-devtools!)
 
 (excercise!)
 
