2da0431e2e437a9f4212f26bb8fe89e8e964dc55
Antonin Hildebrand <antonin@hildebrand.cz>
2015-01-31 12:34:34 +0100

    Various code improvements

--- src/devtools/core.cljs
+++ src/devtools/core.cljs
@@ -3,20 +3,27 @@
 
 (def max-coll-elements 10)
 (def max-map-elements 5)
-(def max-set-elements 7)
+(def max-set-elements 10)
 (def max-seq-elements 20)
 (def abbreviation "…")
 (def line-index-separator ":")
 (def formatter-key "devtoolsFormatter")
 (def dq "\"")
+(def surrogate-key "$$surrogate")
+(def standard-ol-style "list-style-type:none; padding-left:0px; margin-top:0px; margin-bottom:0px; margin-left:12px")
+(def standard-li-style "margin-left:12px")
+(def spacer " ")
+(def span "span")
+(def ol "ol")
+(def li "li")
 
-(def ^:dynamic *devtools-enabled* true)
 (def ^:dynamic *devtools-installed* false)
+(def ^:dynamic *devtools-enabled* true)
 (def ^:dynamic *original-formatter* nil)
 
 (declare inlined-value-template)
 
-; dirty
+; dirty TODO: find a reliable way how to detect cljs values
 (defn cljs-value? [value]
   (or (exists? (aget value "meta"))
       (exists? (aget value "_meta"))
@@ -26,98 +33,99 @@
   (not (cljs-value? value)))
 
 (defn surrogate? [value]
-  (exists? (aget value "__surrogate")))
+  (exists? (aget value surrogate-key)))
 
-(defn template [tag style & more]
-  (let [arr #js [tag (if (empty? style) #js {} #js {"style" style})]]
-    (doseq [o more]
-      (if (seq? o)
-        (.apply (aget arr "push") arr (into-array o)) ; convenience helper to splat cljs-seqs
-        (.push arr o)))
-    arr))
+(defn template [tag style & children]
+  (let [js-array #js [tag (if (empty? style) #js {} #js {"style" style})]]
+    (doseq [child children]
+      (if (coll? child)
+        (.apply (aget js-array "push") js-array (into-array child)) ; convenience helper to splat cljs collections
+        (.push js-array child)))
+    js-array))
 
-(defn reference [object & more]
-  (let [arr #js ["object" #js {"object" object}]]
-    (doseq [o more]
-      (.push arr o))
-    arr))
+(defn reference [object & children]
+  (let [js-array #js ["object" #js {"object" object}]]
+    (doseq [child children]
+      (.push js-array child))
+    js-array))
 
 (defn surrogate [object header]
   (js-obj
-    "__surrogate" true
+    surrogate-key true
     "target" object
     "header" header))
 
-(defn spacer [& _] " ")
-
 (defn nil-template [_]
-  (template "span" "color:#808080" "nil"))
+  (template span "color:#808080" "nil"))
 
 (defn keyword-template [value]
-  (template "span" "color:#881391" (str ":" (name value))))
+  (template span "color:#881391" (str ":" (name value))))
 
 (defn symbol-template [value]
-  (template "span" "color:#000000" (str value)))
+  (template span "color:#000000" (str value)))
 
 (defn number-template [value]
   (if (integer? value)
-    (template "span" "color:#1C00CF" value)
-    (template "span" "color:#1C88CF" value)))
+    (template span "color:#1C00CF" value)
+    (template span "color:#1C88CF" value)))
 
 (defn index-template [value]
-  (template "span" "color:#881391" value line-index-separator))
+  (template span "color:#881391" value line-index-separator))
 
 (defn deref-template [value]
   (cond
-    (satisfies? IAtom value) (template "span" "color:#f0f" "#<Atom " (reference @value) ">")
-    (satisfies? IVolatile value) (template "span" "color:#f0f" "#<Volatile " (reference @value) ">")
+    (satisfies? IAtom value) (template span "color:#f0f" "#<Atom " (reference @value) ">")
+    (satisfies? IVolatile value) (template span "color:#f0f" "#<Volatile " (reference @value) ">")
     :else (pr-str value))) ; TODO: should we handle IDelay and others? I believe it is not safe to dereference them here
 
 ; TODO: abbreviate long strings
 (defn string-template [value]
-  (template "span" "color:#C41A16" (str dq value dq)))
+  (template span "color:#C41A16" (str dq value dq)))
 
 (defn fn-template [value]
-  (template "span" "color:#f0f" (reference (surrogate value "λ"))))
+  (template span "color:#090" (reference (surrogate value "λ"))))
 
 (defn header-inlined-templates [value renderer max]
-  (let [rendered-items (apply concat (interpose [(spacer)] (map renderer (take max value))))]
+  (let [rendered-items (apply concat (interpose [spacer] (map renderer (take max value))))]
     (if (> (count value) max)
       (concat rendered-items [abbreviation])
       rendered-items)))
 
 (defn header-map-template [value]
-  (let [renderer (fn [[key value]] [(inlined-value-template key) (spacer) (inlined-value-template value)])
+  (let [renderer (fn [[key value]] [(inlined-value-template key) spacer (inlined-value-template value)])
         items (header-inlined-templates value renderer max-map-elements)]
-    (template "span" "color:#000" "{" items "}")))
+    (template span "color:#000" "{" items "}")))
 
 (defn header-set-template [value]
   (let [renderer (fn [item] [(inlined-value-template item)])
         items (header-inlined-templates value renderer max-set-elements)]
-    (template "span" "color:#000" "#{" items "}")))
+    (template span "color:#000" "#{" items "}")))
 
 (defn header-seq-template [value]
   (let [renderer (fn [item] [(inlined-value-template item)])
         items (header-inlined-templates value renderer max-seq-elements)]
-    (template "span" "color:#000" "(" items ")")))
+    (template span "color:#000" "(" items ")")))
 
 (defn header-coll-template [value]
   (let [renderer (fn [item] [(inlined-value-template item)])
         items (header-inlined-templates value renderer max-coll-elements)]
-    (template "span" "color:#000" "[" items "]")))
+    (template span "color:#000" "[" items "]")))
 
 (defn bool-template [value]
-  (template "span" "color:#099" value))
+  (template span "color:#099" value))
 
 (defn generic-template [value]
-  (template "span" "color:#000" (reference value)))
+  (template span "color:#000" (reference value)))
 
 (defn js-object-template [value]
   (if (js-value? value)
-    (template "span" "color:#000" (reference (surrogate value "#js")))))
+    (template span "color:#000" (reference (surrogate value "#js"))))) ; TODO: we could render short preview of #js value here
 
 (defn bool? [value]
   (or (true? value) (false? value)))
+
+(defn deref? [value]
+  (satisfies? IDeref value))
 
 (defn atomic-template [value]
   (cond
@@ -128,16 +136,14 @@
     (keyword? value) (keyword-template value)
     (symbol? value) (symbol-template value)
     (fn? value) (fn-template value)
-    (satisfies? IDeref value) (deref-template value)
-    ))
+    (deref? value) (deref-template value)))
 
 (defn header-container-template [value]
   (cond
     (map? value) (header-map-template value)
     (set? value) (header-set-template value)
     (seq? value) (header-seq-template value)
-    (coll? value) (header-coll-template value)
-    ))
+    (coll? value) (header-coll-template value)))
 
 (defn inlined-value-template [value]
   (or (atomic-template value)
@@ -150,24 +156,21 @@
       (pr-str value)))
 
 (defn build-header [value]
-  (template "span" "background-color:#efe" (header-template value)))
+  (template span "background-color:#efe" (header-template value)))
 
 (defn body-line-template [index value]
-  (template "li" "margin-left:12px" (index-template index) (spacer) (inlined-value-template value)))
+  (template li standard-li-style (index-template index) spacer (inlined-value-template value)))
 
 (defn body-line-templates [value]
-  (let [arr #js []]
-    (loop [data (seq value) ; TODO: limit max number of lines here?
-           index 0]
-      (if (empty? data)
-        arr
-        (do
-          (.push arr (body-line-template index (first data)))
-          (recur (rest data) (inc index)))))))
+  (loop [data (seq value) ; TODO: limit max number of lines here?
+         index 0
+         lines []]
+    (if (empty? data)
+      lines
+      (recur (rest data) (inc index) (conj lines (body-line-template index (first data)))))))
 
 (defn build-body [value]
-  (.concat (template "ol" "list-style-type:none; padding-left:0px; margin-top:0px; margin-bottom:0px; margin-left:12px")
-           (body-line-templates value)))
+  (template ol standard-ol-style (body-line-templates value)))
 
 (defn something-abbreviated? [value]
   (if (coll? value)
@@ -193,9 +196,7 @@
 
 (defn build-surrogate-body [value]
   (let [target (.-target value)]
-    (template "ol"
-              "list-style-type:none; padding-left:0px; margin-top:0px; margin-bottom:0px; margin-left:12px"
-              (template "li" "margin-left:12px" (reference target (pr-str target))))))
+    (template ol standard-ol-style (template li standard-li-style (reference target (pr-str target))))))
 
 (defn body-hook [value]
   (if (surrogate? value)
@@ -214,7 +215,7 @@
 
 (defn chain
   "chains our hook with original formatter"
-  [name hook original-formatter]
+  [name original-formatter hook]
   (let [call-original-formatter? (fn [value]
                                    (if (not (nil? original-formatter))
                                      (do ; TODO should we wrap this in try-catch instead?
@@ -226,11 +227,11 @@
         (call-original-formatter? value)))))
 
 (defn cljs-formatter [original-formatter]
-  (let [wrapper (fn [name hook] (debug/hook-monitor name (chain name (sanitize hook) original-formatter)))]
+  (let [hook-wrapper (fn [name hook] (debug/hook-monitor name (chain name original-formatter (sanitize hook))))]
     (js-obj
-      "header" (wrapper "header" header-hook)
-      "hasBody" (wrapper "hasBody" has-body-hook)
-      "body" (wrapper "body" body-hook))))
+      "header" (hook-wrapper "header" header-hook)
+      "hasBody" (hook-wrapper "hasBody" has-body-hook)
+      "body" (hook-wrapper "body" body-hook))))
 
 (defn install-devtools! []
   (if *devtools-installed*
