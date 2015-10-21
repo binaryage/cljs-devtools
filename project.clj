8dc035be0bd61d07e5bfe1fea0382d2a0101bac0
Antonin Hildebrand <antonin@hildebrand.cz>
2015-10-21 13:08:09 +0200

    Bump dependencies and update project structure

--- project.clj
+++ project.clj
@@ -2,28 +2,28 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [binaryage/devtools "0.2.2"]
-                 [cljs-http "0.1.30"]
-                 [ring "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [binaryage/devtools "0.3.0"]
+                 [cljs-http "0.1.37"]
+                 [ring "1.4.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]
-            [lein-ring "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-ring "0.9.7"]]
 
-  :clean-targets ["out"]
+  :clean-targets ^{:protect false} ["resources/public/_compiled"]
 
   :ring {:handler server.core/app}
 
   :source-paths ["src" "target/classes"]
 
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src" "checkouts/cljs-devtools/src"]
-              :compiler {
-                :main devtools-sample.core
-                :output-to "out/devtools_sample.js"
-                :output-dir "out"
-                :optimizations :none
-                :cache-analysis true
-                :source-map true}}]})
+  :cljsbuild {:builds [{:id           "dev"
+                        :source-paths ["src"
+                                       "checkouts/cljs-devtools/src"
+                                       "checkouts/cljs-devtools/src-debug"]
+                        :compiler     {:main           devtools-sample.core
+                                       :output-to      "resources/public/_compiled/devtools_sample.js"
+                                       :output-dir     "resources/public/_compiled"
+                                       :asset-path     "_compiled"
+                                       :optimizations  :none
+                                       :source-map     true}}]})
