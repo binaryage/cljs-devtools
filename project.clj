eda6bf77bf3ca2ea50492d6384460203ddf2b1e6
Antonin Hildebrand <antonin@hildebrand.cz>
2015-10-21 13:16:25 +0200

    Bump dependencies and update project structure

--- project.clj
+++ project.clj
@@ -4,10 +4,14 @@
   :license {:name         "MIT License"
             :url          "http://opensource.org/licenses/MIT"
             :distribution :repo}
+
   :scm {:name "git"
         :url  "https://github.com/binaryage/cljs-devtools"}
+
   :signing {:gpg-key "DDD8C87F"}
+
   :deploy-repositories [["clojars" {:creds :gpg}]]
+
   :pom-addition [:developers [:developer
                               [:name "Antonin Hildebrand"]
                               [:url "https://github.com/darwin"]
@@ -15,15 +19,17 @@
                               [:timezone "+1"]]]
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.107" :scope "provided"]]
+                 [org.clojure/clojurescript "1.7.145" :scope "provided"]]
 
-  :clean-targets ["out"]
+  :clean-targets ^{:protect false} ["out" "test/_generated"]
+
   :plugins [[lein-cljsbuild "1.1.0"]
             [lein-cljfmt "0.1.7"]]
+
   :hooks [leiningen.cljsbuild]
+
   :cljsbuild
-  {:builds
-                  {:dev
+  {:builds        {:dev
                    {:source-paths ["src", "src-debug"]
                     :compiler     {:output-to     "out/dev/cljs_devtools.js"
                                    :output-dir    "out/dev"
@@ -37,16 +43,11 @@
                                    :source-map    "out/prod/cljs_devtools.min.js.map"}}
                    :test
                    {:source-paths ["src", "test"]
-                    :compiler     {:output-to     "out/test/cljs_devtools.test.js"
-                                   :output-dir    "out/test"
+                    :compiler     {:output-to     "test/_generated/cljs_devtools.test.js"
+                                   :output-dir    "test/_generated"
                                    :main          devtools.runner
                                    :asset-path    "_generated"
                                    :optimizations :none
                                    :pretty-print  true
-                                   :source-map    true}}
-                   }
-   :test-commands {"unit" ["phantomjs" "test/phantom.js" "test/runner.html"]}
-   }
-
-  )
-
+                                   :source-map    true}}}
+   :test-commands {"unit" ["phantomjs" "test/phantom.js" "test/runner.html"]}})
