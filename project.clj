a6190f041a85839861db597787f4020adbecf5dd
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-02 01:31:44 +0100

    Move project version to 0.4.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/devtools "0.4.1"
+(defproject binaryage/devtools "0.4.2-SNAPSHOT"
   :description "Experimental Chrome devtools support for ClojureScript"
   :url "https://github.com/binaryage/cljs-devtools"
   :license {:name         "MIT License"
