22b679c0b6200f2a8e944e276c672f2158aab03d
Antonin Hildebrand <antonin@hildebrand.cz>
2015-11-02 01:24:52 +0100

    Bump project version to 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/devtools "0.4.1-SNAPSHOT"
+(defproject binaryage/devtools "0.4.1"
   :description "Experimental Chrome devtools support for ClojureScript"
   :url "https://github.com/binaryage/cljs-devtools"
   :license {:name         "MIT License"
