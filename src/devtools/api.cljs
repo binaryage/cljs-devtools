a03555b817c6338163f3973bf92de40e92a2421a
Antonin Hildebrand <antonin@hildebrand.cz>
2015-12-10 15:55:18 +0100

    safety checks + meaningful error messages

--- src/devtools/api.cljs
+++ src/devtools/api.cljs
@@ -3,6 +3,9 @@
   (:require [goog.object]
             [clojure.string :as string]))
 
+; note we don't want to require figwheel here to force dependency on it for all cljs-devtools consumers
+; instead we do lazy binding during runtime
+
 (def wrapper-template
   "(try
      (js/devtools.api.repl_result {request-id} {code})
@@ -16,13 +19,19 @@
 (defn log-repl-result [request-id & args]
   (apply console-log (concat ["$REPL-RESULT$" request-id] args)))
 
-; TODO: error checks
 (defn ^:export repl-eval [request-id code]
-  (if-let [figwheel-repl-ns (oget js/window "figwheel" "client" "repl")]
-    (let [wrapped-code (-> wrapper-template
-                           (string/replace "{request-id}" request-id)
-                           (string/replace "{code}" code))]
-      (ocall figwheel-repl-ns "repl_eval" request-id wrapped-code code))))
+  (if-let [figwheel-ns (oget js/window "figwheel")]
+    (if-let [figwheel-repl-ns (oget figwheel-ns "client" "repl")]
+      (if (ocall figwheel-repl-ns "is_repl_connected")
+        (if (ocall figwheel-repl-ns "is_repl_available")
+          (let [wrapped-code (-> wrapper-template
+                                 (string/replace "{request-id}" request-id)
+                                 (string/replace "{code}" code))]
+            (ocall figwheel-repl-ns "repl_eval" request-id wrapped-code code))
+          (log-repl-result request-id "Figwheel is connected, but REPL functionality is not available. The REPL feature is enabled by default, but your configuration might have disabled it."))
+        (log-repl-result request-id "Figwheel is present in your project, but not connected to Figwheel server. Please follow Figwheel docs on how to setup it properly => https://github.com/bhauman/lein-figwheel"))
+      (log-repl-result request-id "An old version of Figwheel is present. Please integrate the latest Figwheel into your project => https://github.com/bhauman/lein-figwheel"))
+    (log-repl-result request-id "Figwheel is not present. Please integrate Figwheel into your project => https://github.com/bhauman/lein-figwheel")))
 
 (defn ^:export repl-result [request-id value]
   (log-repl-result request-id value)
