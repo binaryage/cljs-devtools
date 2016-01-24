27158c5b56fb2a1e49d60584b6c56066c9713963
Antonin Hildebrand <antonin@hildebrand.cz>
2016-01-24 22:47:35 +0100

    reference example function

--- src/demo/devtools_sample/core.cljs
+++ src/demo/devtools_sample/core.cljs
@@ -10,12 +10,15 @@
 ; --- MEAT STARTS HERE -->
 ; note: (log ...) function is a shorthand for (.log js/console ...)
 
+(defn hello [name]
+  (str "hello, " name "!"))
+
 (log nil 43 0.1 :keyword 'symbol "string" #"regexp" [1 2 3] #{1 2 3} {:k1 1 :k2 2} #js [1 2 3] #js {"k1" 1 "k2" 2})
 (log [nil 42 0.1 :keyword 'symbol "string" #"regexp" [1 2 3] #{1 2 3} {:k1 1 :k2 2} #js [1 2 3] #js {"k1" 1 "k2" 2} (js/Date.)])
 (log (range 100) (range 101) (range 220) (interleave (repeat :even) (repeat :odd)))
 (log {:k1 'v1 :k2 'v2 :k3 'v3 :k4 'v4 :k5 'v5 :k6 'v6 :k7 'v7 :k8 'v8 :k9 'v9})
 (log #{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15})
-(log [[js/window] (js-obj "k1" "v1" "k2" :v2) #(.log js/console "hello") (js* "function(x) { console.log(x); }")])
+(log [js/window (js-obj "k1" "v1" "k2" :v2) hello (js* "function(x) { console.log(x); }")])
 (log [1 2 3 4 5 [10 20 30 40 50 [100 200 300 400 500 [1000 2000 3000 4000 5000 :*]]]])
 (log [1 2 3 [10 20 30 [100 200 300 [1000 2000 3000 :*]]]])
 (log (atom {:number 0 :string "string" :keyword :keyword :symbol 'symbol :vector [0 1 2 3 4 5 6] :set '#{a b c} :map '{k1 v1 k2 v2}}))
