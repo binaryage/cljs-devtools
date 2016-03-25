ed230b594cfe9224d2b7cc9bd2b8bc1d5a54b958
Antonin Hildebrand <antonin@hildebrand.cz>
2016-03-25 17:14:02 +0100

    move project version to next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/devtools "0.5.3"
+(defproject binaryage/devtools "0.5.4-SNAPSHOT"
   :description "Chrome DevTools utilities for ClojureScript"
   :url "https://github.com/binaryage/cljs-devtools"
   :license {:name         "MIT License"
