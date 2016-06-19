e662a5d826ca944758f7ba5e90cbef6a9a9c490e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-19 23:52:05 +0200

    update project for binaryage/devtools 0.7.1

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/binaryage/cljs-devtools-sample"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.36"]
-                 [binaryage/devtools "0.7.0"]
+                 [org.clojure/clojurescript "1.9.76"]
+                 [binaryage/devtools "0.7.1"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [cljs-http "0.1.41"]
                  [environ "1.0.3"]
-                 [figwheel "0.5.4"]]
+                 [figwheel "0.5.4-3"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-figwheel "0.5.4"]
+            [lein-figwheel "0.5.4-3"]
             [lein-shell "0.5.0"]
             [lein-environ "1.0.3"]]
 
