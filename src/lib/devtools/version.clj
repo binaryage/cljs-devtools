c638f20f39e0478f9b2397e711c3a28d3eacb921
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-21 20:20:28 +0200

    move to next snapshot version

--- src/lib/devtools/version.clj
+++ src/lib/devtools/version.clj
@@ -1,6 +1,6 @@
 (ns devtools.version)
 
-(def current-version "0.7.2")                                                                                                 ; this should match our project.clj
+(def current-version "0.7.3-SNAPSHOT")                                                                                        ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
