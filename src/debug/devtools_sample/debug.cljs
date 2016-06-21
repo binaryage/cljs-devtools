9d3b7bed7412e4c3096faf00f7801c5c2061f277
Antonin Hildebrand <antonin@hildebrand.cz>
2016-06-21 23:17:34 +0200

    move all debug code into debug namespace

--- src/debug/devtools_sample/debug.cljs
+++ src/debug/devtools_sample/debug.cljs
@@ -1,4 +1,10 @@
 (ns devtools-sample.debug
-  (:require [devtools.debug :as debug]))
+  (:require [devtools.debug :as debug]
+            [devtools.custom-formatters :as custom-formatters]))
+
+(log "devtools-sample: enabled debug mode")
+
+(set! custom-formatters/*monitor-enabled* true)
+(set! custom-formatters/*sanitizer-enabled* false)
 
 (debug/init!)
--- src/demo/devtools_sample/boot.cljs
+++ src/demo/devtools_sample/boot.cljs
@@ -1,7 +1,6 @@
 (ns devtools-sample.boot
   (:require-macros [cljs.core.async.macros :refer [go]]
-                   [devtools-sample.logging :refer [log]]
-                   [devtools-sample.config :refer [debug?]])
+                   [devtools-sample.logging :refer [log]])
   (:require [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
             [clojure.string :as string]
@@ -29,10 +28,5 @@
       (.highlightBlock js/hljs block))))
 
 (defn boot! [source-path]
-  (when (debug?)
-    (log "devtools-sample: enabled debug mode")
-    (set! custom-formatters/*monitor-enabled* true)
-    (set! custom-formatters/*sanitizer-enabled* false)
-    #_(js/goog.require "devtools_sample.debug"))
   (enable-console-print!)
   (fetch-source-code source-path))
