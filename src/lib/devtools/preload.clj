55b4d0b587f5ace0e29996a057afd46107495e6a
Antonin Hildebrand <antonin@hildebrand.cz>
2016-07-06 14:51:19 +0200

    read configuration from :external-config, deprecate :tooling-config
    figwheel did the pick here:
    https://github.com/bhauman/lein-figwheel/commit/80f7306bf5e6bd1330287a6f3cc259ff645d899b

--- src/lib/devtools/preload.clj
+++ src/lib/devtools/preload.clj
@@ -2,7 +2,9 @@
 
 (defn read-config []
   (if cljs.env/*compiler*
-    (get-in @cljs.env/*compiler* [:options :tooling-config :devtools/config])))
+    (or
+      (get-in @cljs.env/*compiler* [:options :external-config :devtools/config])                                              ; https://github.com/bhauman/lein-figwheel/commit/80f7306bf5e6bd1330287a6f3cc259ff645d899b
+      (get-in @cljs.env/*compiler* [:options :tooling-config :devtools/config]))))                                            ; :tooling-config is deprecated
 
 (defmacro gen-config []
   (or (read-config) {}))
