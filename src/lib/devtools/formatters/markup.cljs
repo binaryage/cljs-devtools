8b413b7179cc0bb7d796d4a243abf695b124e038
Antonin Hildebrand <antonin@hildebrand.cz>
2016-07-17 22:01:28 +0200

    refactor markup namespace

--- src/lib/devtools/formatters/markup.cljs
+++ src/lib/devtools/formatters/markup.cljs
@@ -11,6 +11,25 @@
 
 (declare get-markup-map)
 
+; -- helpers ----------------------------------------------------------------------------------------------------------------
+; TODO: move these into helpers namespace
+
+(defn- get-more-marker [more-count]
+  (str (pref :plus-symbol) more-count (pref :more-symbol)))
+
+(defn- wrap-arity [arity]
+  (let [args-open-symbol (pref :args-open-symbol)
+        args-close-symbol (pref :args-close-symbol)]
+    (str args-open-symbol arity args-close-symbol)))
+
+(defn- fetch-field-value [obj field]
+  [field (oget obj (munge field))])
+
+(defn- fetch-fields-values [obj fields]
+  (map (partial fetch-field-value obj) fields))
+
+; -- references -------------------------------------------------------------------------------------------------------------
+
 (defn <surrogate> [& args]
   (concat ["surrogate"] args))
 
@@ -20,8 +39,14 @@
 (defn <reference-surrogate> [& args]
   (<reference> (apply <surrogate> args)))
 
-(defn <preview> [value]
-  (managed-pr-str value :header-style (pref :max-print-level) (get-markup-map)))
+(defn <circular-reference> [& children]
+  (concat [:circular-reference-tag :circular-ref-icon] children))
+
+(defn <native-reference> [object]
+  (let [reference (<reference> object {:prevent-recursion true})]
+    [:native-reference-tag :native-reference-background reference]))
+
+; -- simple markup ----------------------------------------------------------------------------------------------------------
 
 (defn <cljs-land> [& children]
   (concat [:cljs-land-tag] children))
@@ -43,12 +68,7 @@
     [:integer-tag number]
     [:float-tag number]))
 
-(defn <circular-reference> [& children]
-  (concat [:circular-reference-tag :circular-ref-icon] children))
-
-(defn <native-reference> [object]
-  (let [reference (<reference> object {:prevent-recursion true})]
-    [:native-reference-tag :native-reference-background reference]))
+; -- string markup ----------------------------------------------------------------------------------------------------------
 
 (defn <string> [string]
   (let [dq (pref :dq)
@@ -65,35 +85,79 @@
                                                        (pref :string-postfix-limit))
             abbreviated-string-markup [:string-tag (quote-string abbreviated-string)]
             string-with-nl-markers (.replace string re-nl (str nl-marker "\n"))
-            body-markup [:expanded-string-tag string-with-nl-markers]]
-        (<reference-surrogate> string abbreviated-string-markup true body-markup))
+            details-markup [:expanded-string-tag string-with-nl-markers]]
+        (<reference-surrogate> string abbreviated-string-markup true details-markup))
       [:string-tag (quote-string inline-string)])))
 
-(defn <meta> [metadata]
-  (let [body [:meta-body-tag (<preview> metadata)]
-        header [:meta-header-tag "meta"]]
-    [:meta-reference-tag (<reference-surrogate> metadata header true body)]))
+; -- generic preview markup -------------------------------------------------------------------------------------------------
 
-(defn <meta-wrapper> [metadata & children]
-  (concat [:meta-wrapper-tag] children [(<meta> metadata)]))
+(defn <preview> [value]
+  (managed-pr-str value :header-style (pref :max-print-level) (get-markup-map)))
+
+; -- body-related templates -------------------------------------------------------------------------------------------------
+
+(defn <aligned-body> [markups-lists]
+  (let [prepend-li-tag (fn [line]
+                         (if line
+                           (concat [:aligned-li-tag] line)))
+        aligned-lines (keep prepend-li-tag markups-lists)]
+    [:body-tag
+     (concat [:standard-ol-no-margin-tag] aligned-lines)]))
+
+(defn <standard-body> [markups-lists & [no-margin?]]
+  (let [ol-tag (if no-margin? :standard-ol-no-margin-tag :standard-ol-tag)
+        li-tag (if no-margin? :standard-li-no-margin-tag :standard-li-tag)
+        prepend-li-tag (fn [line]
+                         (if line
+                           (concat [li-tag] line)))
+        lines-markups (keep prepend-li-tag markups-lists)]
+    (concat [ol-tag] lines-markups)))
+
+(defn <standard-body-reference> [o]
+  (<standard-body> [[(<reference> o)]]))
+
+; -- generic details markup -------------------------------------------------------------------------------------------------
 
 (defn <index> [value]
   [:index-tag value :line-index-separator])
 
-(defn <aligned-body> [lines]
-  (let [align (fn [line]
-                (if line
-                  (concat [:aligned-li-tag] line)))
-        aligned-lines (keep align lines)]
-    [:body-tag
-     (concat [:standard-ol-no-margin-tag] aligned-lines)]))
+(defn- body-line [index value]
+  [(<index> index) (managed-pr-str value :item-style (pref :body-line-max-print-level) (get-markup-map))])
+
+; TODO: this fn is screaming for rewrite
+(defn- prepare-body-lines [data starting-index]
+  (loop [work data
+         index starting-index
+         lines []]
+    (if (empty? work)
+      lines
+      (recur (rest work) (inc index) (conj lines (body-line index (first work)))))))
+
+(defn- body-lines [value starting-index]
+  (let [seq (seq value)
+        max-number-body-items (pref :max-number-body-items)
+        chunk (take max-number-body-items seq)
+        rest (drop max-number-body-items seq)
+        lines (prepare-body-lines chunk starting-index)
+        continue? (not (empty? (take 1 rest)))]
+    (if-not continue?
+      lines
+      (let [more-label-markup [:body-items-more-tag (pref :body-items-more-label)]
+            start-index (+ starting-index max-number-body-items)
+            more-markup (<reference-surrogate> rest more-label-markup true nil start-index)]
+        (conj lines [more-markup])))))
+
+(defn <details> [value starting-index]
+  (let [has-continuation? (pos? starting-index)
+        body-markup (<standard-body> (body-lines value starting-index) has-continuation?)]
+    (if has-continuation?
+      body-markup
+      [:body-tag body-markup])))
+
+; -- generic list template --------------------------------------------------------------------------------------------------
 
 (defn <list-details> [items _opts]
-  (let [wrap (fn [x] [x])]
-    (<aligned-body> (map wrap items))))
-
-(defn- get-more-marker [more-count]
-  (str (pref :plus-symbol) more-count (pref :more-symbol)))
+  (<aligned-body> (map list items)))
 
 (defn <list> [items max-count & [opts]]
   (let [items-markups (take max-count items)
@@ -117,27 +181,28 @@
         (<reference-surrogate> nil preview-markup true (or details-markup default-details-fn)))
       preview-markup)))
 
-; ---------------------------------------------------------------------------------------------------------------------------
+; -- mete-related markup ----------------------------------------------------------------------------------------------------
 
-(defn- wrap-arity [arity]
-  (let [args-open-symbol (pref :args-open-symbol)
-        args-close-symbol (pref :args-close-symbol)]
-    (str args-open-symbol arity args-close-symbol)))
+(defn <meta> [metadata]
+  (let [body [:meta-body-tag (<preview> metadata)]
+        header [:meta-header-tag "meta"]]
+    [:meta-reference-tag (<reference-surrogate> metadata header true body)]))
 
-(defn <arity> [prefix arity]
-  [[:fn-multi-arity-args-indent-tag prefix]
-   [:fn-args-tag arity]])
+(defn <meta-wrapper> [metadata & children]
+  (concat [:meta-wrapper-tag] children [(<meta> metadata)]))
+
+; -- function markup --------------------------------------------------------------------------------------------------------
 
 (defn <function-details> [fn-obj ns _name arities prefix]
   {:pre [(fn? fn-obj)]}
   (let [arities (map wrap-arity arities)
-        arities (if (> (count arities) 1)
-                  (map (partial <arity> prefix) arities))
-        ns (if-not (empty? ns)
-             [:ns-icon
-              [:fn-ns-name-tag ns]])
-        native [:native-icon (<native-reference> fn-obj)]]
-    (<aligned-body> (concat arities [ns native]))))
+        make-arity-markup-list (fn [arity]
+                                 [[:fn-multi-arity-args-indent-tag prefix]
+                                  [:fn-args-tag arity]])
+        arities-markupts-lists (if (> (count arities) 1) (map make-arity-markup-list arities))
+        ns-markups-list (if-not (empty? ns) [:ns-icon [:fn-ns-name-tag ns]])
+        native-markups-list [:native-icon (<native-reference> fn-obj)]]
+    (<aligned-body> (concat arities-markupts-lists [ns-markups-list native-markups-list]))))
 
 (defn <arities> [arities]
   (let [multi-arity? (> (count arities) 1)]
@@ -148,20 +213,20 @@
 (defn <function> [fn-obj]
   {:pre [(fn? fn-obj)]}
   (let [[ns name] (munging/parse-fn-info fn-obj)
+        lambda? (empty? name)
         spacer-symbol (pref :spacer)
         rest-symbol (pref :rest-symbol)
         multi-arity-symbol (pref :multi-arity-symbol)
         arities (munging/extract-arities fn-obj true spacer-symbol multi-arity-symbol rest-symbol)
         arities-markup (<arities> arities)
-        lambda? (empty? name)
         name-markup (if-not lambda? [:fn-name-tag name])
-        icon (if lambda? :lambda-icon :fn-icon)
-        prefix [:fn-prefix-tag icon name-markup]
-        preview [:fn-header-tag prefix arities-markup]
-        details (partial <function-details> fn-obj ns name arities prefix)]
-    (<reference-surrogate> fn-obj preview true details)))
+        icon-markup (if lambda? :lambda-icon :fn-icon)
+        prefix-markup [:fn-prefix-tag icon-markup name-markup]
+        preview-markup [:fn-header-tag prefix-markup arities-markup]
+        details-fn (partial <function-details> fn-obj ns name arities prefix-markup)]
+    (<reference-surrogate> fn-obj preview-markup true details-fn)))
 
-; ---------------------------------------------------------------------------------------------------------------------------
+; -- type markup ------------------------------------------------------------------------------------------------------------
 
 (defn <type-basis-item> [basis-item]
   [:type-basis-item-tag (name basis-item)])
@@ -189,41 +254,7 @@
 (defn <standalone-type> [constructor-fn & [header-style]]
   [:standalone-type-tag (<type> constructor-fn header-style)])
 
-; ---------------------------------------------------------------------------------------------------------------------------
-
-(defn- fetch-field-value [obj field]
-  [field (oget obj (munge field))])
-
-(defn- fetch-fields-values [obj fields]
-  (map (partial fetch-field-value obj) fields))
-
-(defn <field> [field]
-  (let [[name value] field]
-    [:header-field-tag
-     [:header-field-name-tag (str name)]
-     :header-field-value-spacer
-     [:header-field-value-tag (<reference> value)]
-     :header-field-separator]))
-
-(defn <fields-details-row> [field]
-  (let [[name value] field]
-    [:body-field-tr-tag
-     [:body-field-td1-tag
-      :body-field-symbol
-      [:body-field-name-tag (str name)]]
-     [:body-field-td2-tag
-      :body-field-value-spacer
-      [:body-field-value-tag (<reference> value)]]]))
-
-(defn <fields> [fields & [max-fields]]
-  (let [max-fields (or max-fields (pref :max-instance-header-fields))
-        fields-markups (map <field> (take max-fields fields))
-        more? (> (count fields) max-fields)]
-    (concat [:fields-header-tag
-             :fields-header-open-symbol]
-            fields-markups
-            [(if more? :more-fields-symbol)
-             :fields-header-close-symbol])))
+; -- protocols markup -------------------------------------------------------------------------------------------------------
 
 (defn <protocol-method-arity> [arity-fn]
   (<reference> arity-fn))
@@ -233,31 +264,31 @@
 
 (defn <protocol-method-arities> [fns & [max-fns]]
   (let [max-fns (or max-fns (pref :max-protocol-method-arities-list))
-        aritites-markups (map <protocol-method-arity> (take max-fns fns))
         more? (> (count fns) max-fns)
+        aritites-markups (map <protocol-method-arity> (take max-fns fns))
         preview-markup (concat [:protocol-method-arities-header-tag :protocol-method-arities-header-open-symbol]
                                (interpose :protocol-method-arities-list-header-separator aritites-markups)
                                (if more? [:protocol-method-arities-more-symbol])
                                [:protocol-method-arities-header-close-symbol])]
     (if more?
       (let [details-markup-fn (partial <protocol-method-arities-details> fns)]
-        (<reference-surrogate> #js {} preview-markup true details-markup-fn))
+        (<reference-surrogate> nil preview-markup true details-markup-fn))
       preview-markup)))
 
-(defn <protocol-method> [[name fns]]
+(defn <protocol-method> [name arities]
   [:protocol-method-tag
    :method-icon
    [:protocol-method-name-tag name]
-   (<protocol-method-arities> fns)])
+   (<protocol-method-arities> arities)])
 
 (defn <protocol-details> [obj ns _name selector _fast?]
-  (let [ns-markup (if-not (empty? ns) [:ns-icon [:protocol-ns-name-tag ns]])
-        protocol-obj (munging/get-protocol-object selector)
-        native-markup (if (some? protocol-obj) [:native-icon (<native-reference> protocol-obj)])
+  (let [protocol-obj (munging/get-protocol-object selector)
+        ns-markups-list (if-not (empty? ns) [:ns-icon [:protocol-ns-name-tag ns]])
+        native-markups-list (if (some? protocol-obj) [:native-icon (<native-reference> protocol-obj)])
         methods (munging/collect-protocol-methods obj selector)
-        methods-markups (map <protocol-method> methods)
-        wrap (fn [x] [x])]
-    (<aligned-body> (concat (map wrap methods-markups) [ns-markup native-markup]))))
+        methods-markups (map (fn [[name arities]] (<protocol-method> name arities)) methods)
+        methods-markups-lists (map list methods-markups)]
+    (<aligned-body> (concat methods-markups-lists [ns-markups-list native-markups-list]))))
 
 (defn <protocol> [obj protocol & [style]]
   (let [{:keys [ns name selector fast?]} protocol
@@ -273,13 +304,42 @@
     (<protocol> nil fake-protocol :protocol-more-style)))
 
 (defn <protocols-list> [obj protocols & [max-protocols]]
-  (let [max-count (or max-protocols (pref :max-list-protocols))
-        markups (map (partial <protocol> obj) protocols)]
-    (<list> markups max-count {:tag          :protocols-header-tag
-                               :open-symbol  :protocols-list-open-symbol
-                               :close-symbol :protocols-list-close-symbol
-                               :separator    :header-protocol-separator
-                               :more-symbol  <more-protocols>})))
+  (let [max-protocols (or max-protocols (pref :max-list-protocols))
+        protocols-markups (map (partial <protocol> obj) protocols)]
+    (<list> protocols-markups max-protocols {:tag          :protocols-header-tag
+                                             :open-symbol  :protocols-list-open-symbol
+                                             :close-symbol :protocols-list-close-symbol
+                                             :separator    :header-protocol-separator
+                                             :more-symbol  <more-protocols>})))
+
+; -- instance fields markup -------------------------------------------------------------------------------------------------
+
+(defn <field> [name value]
+  [:header-field-tag
+   [:header-field-name-tag (str name)]
+   :header-field-value-spacer
+   [:header-field-value-tag (<reference> value)]
+   :header-field-separator])
+
+(defn <fields-details-row> [field]
+  (let [[name value] field]
+    [:body-field-tr-tag
+     [:body-field-td1-tag
+      :body-field-symbol
+      [:body-field-name-tag (str name)]]
+     [:body-field-td2-tag
+      :body-field-value-spacer
+      [:body-field-value-tag (<reference> value)]]]))
+
+(defn <fields> [fields & [max-fields]]
+  (let [max-fields (or max-fields (pref :max-instance-header-fields))
+        more? (> (count fields) max-fields)
+        fields-markups (map (fn [[name value]] (<field> name value)) (take max-fields fields))]
+    (concat [:fields-header-tag
+             :fields-header-open-symbol]
+            fields-markups
+            [(if more? :more-fields-symbol)
+             :fields-header-close-symbol])))
 
 (defn <fields-details> [fields obj]
   (let [protocols (munging/scan-protocols obj)
@@ -289,11 +349,13 @@
         native-markup [:native-icon (<native-reference> obj)]]
     (<aligned-body> [fields-markup protocols-list-markup native-markup])))
 
+; -- type/record instance markup --------------------------------------------------------------------------------------------
+
 (defn <instance> [value]
   (let [constructor-fn (get-constructor value)
         [_ns _name basis] (munging/parse-constructor-info constructor-fn)
         custom-printing? (implements? IPrintWithWriter value)
-        type-template (<type> constructor-fn :instance-type-header-style)
+        type-markup (<type> constructor-fn :instance-type-header-style)
         fields (fetch-fields-values value basis)
         fields-markup (<fields> fields (if custom-printing? 0))                                                               ; TODO: handle no fields properly
         fields-details-markup-fn #(<fields-details> fields value)
@@ -303,53 +365,11 @@
                                   :instance-custom-printing-background
                                   (managed-print-via-protocol value :instance-custom-printing-style (get-markup-map))])
         preview-markup [:instance-header-tag
-                        type-template
+                        type-markup
                         :instance-value-separator
                         fields-preview-markup
                         custom-printing-markup]]
     (<reference-surrogate> value preview-markup false)))
-
-; ---------------------------------------------------------------------------------------------------------------------------
-
-(defn <standard-body> [lines & [no-margin?]]
-  (let [ol-tag (if no-margin? :standard-ol-no-margin-tag :standard-ol-tag)
-        li-tag (if no-margin? :standard-li-no-margin-tag :standard-li-tag)]
-    (concat [ol-tag] (map #(concat [li-tag] %) lines))))
-
-(defn- body-line [index value]
-  [(<index> index) (managed-pr-str value :item-style (pref :body-line-max-print-level) (get-markup-map))])
-
-(defn- prepare-body-lines [data starting-index]
-  (loop [work data
-         index starting-index
-         lines []]
-    (if (empty? work)
-      lines
-      (recur (rest work) (inc index) (conj lines (body-line index (first work)))))))
-
-(defn- body-lines [value starting-index]
-  (let [seq (seq value)
-        max-number-body-items (pref :max-number-body-items)
-        chunk (take max-number-body-items seq)
-        rest (drop max-number-body-items seq)
-        lines (prepare-body-lines chunk starting-index)
-        continue? (not (empty? (take 1 rest)))]
-    (if-not continue?
-      lines
-      (let [more-label [:body-items-more-tag (pref :body-items-more-label)]
-            start-index (+ starting-index max-number-body-items)
-            more (<reference-surrogate> rest more-label true nil start-index)]
-        (conj lines [more])))))
-
-(defn <details> [value starting-index]
-  (let [continuation? (pos? starting-index)
-        body (<standard-body> (body-lines value starting-index) continuation?)]
-    (if continuation?
-      body
-      [:body-tag body])))
-
-(defn <standard-body-reference> [o]
-  (<standard-body> [[(<reference> o)]]))
 
 ; ---------------------------------------------------------------------------------------------------------------------------
 
