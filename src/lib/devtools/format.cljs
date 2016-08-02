62e412f03255a29747b0b37b6731062fb483b5c2
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-02 13:52:14 +0200

    reimplement some old apis in devtools.format and mark them as deprecated

--- src/lib/devtools/format.cljs
+++ src/lib/devtools/format.cljs
@@ -1,4 +1,5 @@
-(ns devtools.format)
+(ns devtools.format
+  (:require-macros [devtools.util :refer [oget oset ocall oapply safe-call]]))
 
 ; WARNING this namespace is here for legacy reasons, it will be removed in future!
 
@@ -9,3 +10,77 @@
   (-header [value])
   (-has-body [value])
   (-body [value]))
+
+; -- helpers ----------------------------------------------------------------------------------------------------------------
+
+(def ^:dynamic *setup-done*)
+
+(defn setup! []
+  (when-not *setup-done*
+    (set! *setup-done* true)
+
+    ; note: we cannote require devtools.formatters.templating or .markup because that would lead to circular requires
+    (def make-template-fn (oget js/window "devtools" "formatters" "templating" "make_template"))
+    (def make-group-fn (oget js/window "devtools" "formatters" "templating" "make_group"))
+    (def make-reference-fn (oget js/window "devtools" "formatters" "templating" "make_reference"))
+    (def make-surrogate-fn (oget js/window "devtools" "formatters" "templating" "make_surrogate"))
+    (def render-markup-fn (oget js/window "devtools" "formatters" "templating" "render_markup"))
+    (def <header>-fn (oget js/window "devtools" "formatters" "markup" "_LT_header_GT_"))
+    (def <standard-body>-fn (oget js/window "devtools" "formatters" "markup" "_LT_standard_body_GT_"))
+
+    (assert make-template-fn)
+    (assert make-group-fn)
+    (assert make-reference-fn)
+    (assert make-surrogate-fn)
+    (assert render-markup-fn)
+    (assert <header>-fn)
+    (assert <standard-body>-fn)))
+
+(defn- render-markup [& args]
+  (setup!)
+  (apply render-markup-fn args))
+
+; ---------------------------------------------------------------------------------------------------------------------------
+
+; deprecated functionality, implemented for easier transition from v0.7.x to v0.8
+
+(defn ^:deprecated make-template [& args]
+  (setup!)
+  (apply make-template-fn args))
+
+(defn ^:deprecated make-group [& args]
+  (setup!)
+  (apply make-group-fn args))
+
+(defn ^:deprecated make-surrogate [& args]
+  (setup!)
+  (apply make-surrogate-fn args))
+
+(defn ^:deprecated template [& args]
+  (setup!)
+  (apply make-template-fn args))
+
+(defn ^:deprecated group [& args]
+  (setup!)
+  (apply make-group-fn args))
+
+(defn ^:deprecated surrogate [& args]
+  (setup!)
+  (apply make-surrogate-fn args))
+
+(defn ^:deprecated reference [object & [state-override]]
+  (setup!)
+  (apply make-reference-fn [object #(merge % state-override)]))
+
+(defn ^:deprecated standard-reference [target]
+  (setup!)
+  (make-template-fn :ol :standard-ol-style (make-template-fn :li :standard-li-style (make-reference-fn target))))
+
+(defn ^:deprecated build-header [& args]
+  (setup!)
+  (render-markup (apply <header>-fn args)))
+
+(defn ^:deprecated standard-body-template [lines & rest]
+  (setup!)
+  (let [args (concat [(map (fn [x] [x]) lines)] rest)]
+    (render-markup (apply <standard-body>-fn args))))
