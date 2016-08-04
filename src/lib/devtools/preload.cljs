ae77191cb461012d94e2a0562009aaf77cb86aed
Antonin Hildebrand <antonin@hildebrand.cz>
2016-08-04 19:06:28 +0200

    add doc links to devtools.preload

--- src/lib/devtools/preload.cljs
+++ src/lib/devtools/preload.cljs
@@ -5,6 +5,8 @@
 
 ; this namespace is intended to be included in cljs compiler :preloads
 ; overrides for default configuration can be specified in :external-config > :devtools/config
+; see https://github.com/binaryage/cljs-devtools/blob/master/docs/installation.md#install-it-via-preloads
+;     https://github.com/binaryage/cljs-devtools/blob/master/docs/configuration.md#configuration-via-preloads
 
 (def config (gen-config))
 
