fcb8cf4f7b79e5e961b819220823a7467f92797e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-16 20:31:34 +0200

    move to next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject binaryage/devtools "0.8.2"
+(defproject binaryage/devtools "0.8.3-SNAPSHOT"
   :description "A collection of Chrome DevTools enhancements for ClojureScript developers."
   :url "https://github.com/binaryage/cljs-devtools"
   :license {:name         "MIT License"
