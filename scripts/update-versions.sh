a6b5c60774f72d3465dfa80d0fe02f2b56cac5ec
Antonin Hildebrand <antonin@hildebrand.cz>
2016-09-25 15:39:28 +0200

    scripts: fix sed inplace arg

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -17,8 +17,8 @@ if [ -z "$VERSION" ] ; then
   exit 1
 fi
 
-sed -i -e "s/defproject binaryage\/devtools \".*\"/defproject binaryage\/devtools \"$VERSION\"/g" "$PROJECT_FILE"
-sed -i -e "s/def current-version \".*\"/def current-version \"$VERSION\"/g" "$PROJECT_VERSION_FILE"
+sed -i "" -e "s/defproject binaryage\/devtools \".*\"/defproject binaryage\/devtools \"$VERSION\"/g" "$PROJECT_FILE"
+sed -i "" -e "s/def current-version \".*\"/def current-version \"$VERSION\"/g" "$PROJECT_VERSION_FILE"
 
 # this is just a sanity check
 ./scripts/check-versions.sh
