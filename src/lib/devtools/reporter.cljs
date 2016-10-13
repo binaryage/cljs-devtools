898addeac2b4fa0c014fba982386735f0400da8e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-13 20:53:52 +0200

    be more defensive in `report-internal-error!`

--- src/lib/devtools/reporter.cljs
+++ src/lib/devtools/reporter.cljs
@@ -7,25 +7,28 @@
 ; -- internal errors  -------------------------------------------------------------------------------------------------------
 
 (defn report-internal-error! [e & [context footer]]
-  (let [message (if (instance? js/Error e)
-                  (or (.-message e) e)
-                  e)
-        header #js ["%cCLJS DevTools Error%c%s"
-                    "background-color:red;color:white;font-weight:bold;padding:0px 3px;border-radius:2px;"
-                    "color:red"
-                    (str " " message)]
-        context-msg (str "In " (util/get-lib-info) (if context (str ", " context ".") ".") "\n\n")
-        footer-msg (if (some? footer)
-                     footer
-                     (str "\n\n" "---\n" "Please report the issue here: " issues-url))
-        details #js [context-msg e footer-msg]]
-    (let [c js/console
-          group-collapsed (oget c "groupCollapsed")
-          log (oget c "log")
-          group-end (oget c "groupEnd")]
-      (assert group-collapsed)
-      (assert log)
-      (assert group-end)
-      (.apply group-collapsed c header)
-      (.apply log c details)
-      (.call group-end c))))
+  (try
+    (let [message (if (instance? js/Error e)
+                    (or (.-message e) e)
+                    e)
+          header #js ["%cCLJS DevTools Error%c%s"
+                      "background-color:red;color:white;font-weight:bold;padding:0px 3px;border-radius:2px;"
+                      "color:red"
+                      (str " " message)]
+          context-msg (str "In " (util/get-lib-info) (if context (str ", " context ".") ".") "\n\n")
+          footer-msg (if (some? footer)
+                       footer
+                       (str "\n\n" "---\n" "Please report the issue here: " issues-url))
+          details #js [context-msg e footer-msg]]
+      (let [c js/console
+            group-collapsed (oget c "groupCollapsed")
+            log (oget c "log")
+            group-end (oget c "groupEnd")]
+        (assert group-collapsed)
+        (assert log)
+        (assert group-end)
+        (.apply group-collapsed c header)
+        (.apply log c details)
+        (.call group-end c)))
+    (catch :default e
+      (.error js/console "FATAL: report-internal-error! failed" e))))
