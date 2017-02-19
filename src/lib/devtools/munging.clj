dff7ffefbc2562997386304f2bcb4c7f3a9c1494
Antonin Hildebrand <antonin@hildebrand.cz>
2017-02-19 18:52:21 +0100

    work around fast-path-protocols being newly flagged as private
    https://github.com/clojure/clojurescript/commit/627f7fd513d928531db48392d8f52142fea5eb38

--- src/lib/devtools/munging.clj
+++ src/lib/devtools/munging.clj
@@ -1,8 +1,16 @@
 (ns devtools.munging
   (:require [clojure.string :as string]))
 
+(defn steal-fast-path-protocols []
+  ; fast-path-protocols made private in https://github.com/clojure/clojurescript/commit/627f7fd513d928531db48392d8f52142fea5eb38
+  @#'cljs.core/fast-path-protocols)
+
+(defn steal-fast-path-protocol-partitions-count []
+  ; fast-path-protocol-partitions-count made private in https://github.com/clojure/clojurescript/commit/627f7fd513d928531db48392d8f52142fea5eb38
+  @#'cljs.core/fast-path-protocol-partitions-count)
+
 (defmacro get-fast-path-protocols []
-  (pr-str cljs.core/fast-path-protocols))
+  (pr-str (steal-fast-path-protocols)))
 
 (defn get-protocol-info [protocol]
   (let [full-name (str protocol)
@@ -11,11 +19,11 @@
     [ns name selector]))
 
 (defmacro get-fast-path-protocols-lookup-table []
-  (let [protocols cljs.core/fast-path-protocols
+  (let [protocols (steal-fast-path-protocols)
         * (fn [accum [protocol [partition bit]]]
             (update accum partition (fn [partition-map]
                                       (assoc partition-map bit (get-protocol-info protocol)))))]
     (reduce * {} protocols)))
 
 (defmacro get-fast-path-protocol-partitions-count []
-  cljs.core/fast-path-protocol-partitions-count)
+  (steal-fast-path-protocol-partitions-count))
