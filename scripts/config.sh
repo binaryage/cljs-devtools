5659b91f901d74273a7e681861d751106917a0cf
Antonin Hildebrand <antonin@hildebrand.cz>
2017-04-12 01:35:53 +0200

    scripts: introduce CACHE_DIR and DCE_COMPARE_DIR

--- scripts/config.sh
+++ scripts/config.sh
@@ -15,8 +15,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"; cd ..
 ROOT=`pwd`
 PROJECT_VERSION_FILE="src/lib/devtools/version.clj"
 PROJECT_FILE="project.clj"
-DCE_CACHE_DIR="$ROOT/.cache/dce"
-DCE_COMPARE_DIR="$ROOT/test/resources/.compiled/dead-code-compare"
+CACHE_DIR="$ROOT/.cache"
+DCE_CACHE_DIR="$CACHE_DIR/dce"
+DCE_COMPARE_DIR="$CACHE_DIR/dce-compare"
 SKIP_DCE_COMPILATION=
 
 popd
