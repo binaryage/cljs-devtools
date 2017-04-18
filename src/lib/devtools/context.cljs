00fce369e4bd06022d1c10b33e8cc884f0e1544f
Antonin Hildebrand <antonin@hildebrand.cz>
2017-04-19 00:52:04 +0200

    make get-root use goog/global - it is always available

--- src/lib/devtools/context.cljs
+++ src/lib/devtools/context.cljs
@@ -2,5 +2,4 @@
 
 ; default root javascript context, typically js/window in browser, or the root javascript object in node.js
 (defn ^:dynamic get-root []
-  (if (exists? js/goog)
-    js/goog.global))
+  goog/global)
