4b8fa1b3f11dbe53ab36a0abafcd91d00b4ac299
Antonin Hildebrand <antonin@hildebrand.cz>
2017-04-19 01:09:44 +0200

    bump project version to 0.9.4

--- src/lib/devtools/version.clj
+++ src/lib/devtools/version.clj
@@ -1,6 +1,6 @@
 (ns devtools.version)
 
-(def current-version "0.9.4-SNAPSHOT")                                                                                        ; this should match our project.clj
+(def current-version "0.9.4")                                                                                        ; this should match our project.clj
 
 (defmacro get-current-version []
   current-version)
