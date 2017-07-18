7f65435a183a8c4d96f3e676fa4c7ee01e9accd6
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-18 18:44:27 +0200

    tests: silence console output in testenv

--- test/src/tests/devtools/testenv.cljs
+++ test/src/tests/devtools/testenv.cljs
@@ -1,7 +1,18 @@
-(ns devtools.testenv)
+(ns devtools.testenv
+  (:require [devtools.context :as context]))
 
 ; this namespace gets included prior devtools.runner
 
 (enable-console-print!)
 
 (println "ClojureScript version:" *clojurescript-version*)
+
+(def silent-console
+  #js {
+       "info"  (fn [& _args])                                                                                                 ; silent
+       "log"   (fn [& _args])                                                                                                 ; silent
+       "warn"  (fn [& _args])                                                                                                 ; silent
+       "error" (fn [& args]
+                 (.apply js/console.error js/console (into-array args)))})
+
+(set! context/get-console (fn [] silent-console))
