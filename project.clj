binaryage/cljs-devtools

commit 45c1df1e0de53c9d320963b296bd7a741056599c

add support for canary tests

project.clj
@@ -1,3 +1,4 @@
+(def clojurescript-version (or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.9.518"))
 (defproject binaryage/devtools "0.9.5-SNAPSHOT"
   :description "A collection of Chrome DevTools enhancements for ClojureScript developers."
   :url "https://github.com/binaryage/cljs-devtools"
@@ -9,7 +10,7 @@
         :url  "https://github.com/binaryage/cljs-devtools"}
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.518" :scope "provided"]
+                 [org.clojure/clojurescript ~clojurescript-version :scope "provided"]
                  [binaryage/env-config "0.2.0"]]
 
   :clean-targets ^{:protect false} ["target"

.travis.yml
@@ -15,3 +15,4 @@ cache:
   - "$HOME/.m2"
 before_install:
 - yes y | sudo lein upgrade
+- curl -sSL https://raw.githubusercontent.com/cljs-oss/canary/master/scripts/install-canary.sh | bash

test/src/tests/devtools/testenv.cljs
@@ -3,3 +3,5 @@
 ; this namespace gets included prior devtools.runner
 
 (enable-console-print!)
+
+(println "ClojureScript version:" *clojurescript-version*)

commit b18b63835fa7258b06003b2e0f44f3031a1f662e

project: remove broken dependencies setup in :lib profile
This caused that release v0.9.5 had empty dependencies.

I broke it in 45c1df1e0de53c9d320963b296bd7a741056599c.

project.clj
@@ -34,11 +34,7 @@
              :lib
              ^{:pom-scope :provided}                                                                                          ; ! to overcome default jar/pom behaviour, our :dependencies replacement would be ignored for some reason
              [:nuke-aliases
-              {:dependencies   ~(let [project (->> "project.clj" slurp read-string (drop 3) (apply hash-map))
-                                      test-dep? #(->> % (drop 2) (apply hash-map) :scope (= "test"))
-                                      non-test-deps (remove test-dep? (:dependencies project))]
-                                  (with-meta (vec non-test-deps) {:replace true}))                                            ; so ugly!
-               :source-paths   ^:replace ["src/lib"]
+              {:source-paths   ^:replace ["src/lib"]
                :resource-paths ^:replace []
                :test-paths     ^:replace []}]
 