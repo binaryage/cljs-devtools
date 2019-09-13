52bd130eadee2cd74ac1cffa720277fd84440db9
Antonin Hildebrand <antonin@hildebrand.cz>
2019-09-14 00:00:42 +0200

    examples/lein: stop using environ library

--- examples/lein/src/demo/devtools_sample/config.clj
+++ examples/lein/src/demo/devtools_sample/config.clj
@@ -1,5 +1,10 @@
-(ns devtools-sample.config
-  (:require [environ.core :refer [env]]))
+(ns devtools-sample.config)
+
+(defn- read-system-env []
+  (->> (System/getenv)
+       (into {})))
+
+(def env (read-system-env))
 
 (defmacro debug? []
-  (boolean (env :devtools-debug)))
+  (boolean (env "DEVTOOLS_DEBUG")))
