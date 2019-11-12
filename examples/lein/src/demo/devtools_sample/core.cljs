db79b4a353e491a0ccc04ce4e1d2c0ecb754f3a7
Antonin Hildebrand <antonin@hildebrand.cz>
2019-11-12 23:16:29 +0100

    examples: both wrapped and unwrapped values example should be the same

--- examples/lein/src/demo/devtools_sample/core.cljs
+++ examples/lein/src/demo/devtools_sample/core.cljs
@@ -41,7 +41,7 @@
   (-has-body [_] (some? address))
   (-body [_] (render-markup (<standard-body> (string/split-lines address)))))
 
-(log nil 43 0.1 :keyword 'symbol "string" #"regexp" [1 2 3] {:k1 1 :k2 2} #{1 2 3})                                           ; simple values
+(log nil 42 0.1 :keyword 'symbol "string" #"regexp" [1 2 3] {:k1 1 :k2 2} #{1 2 3})                                           ; simple values
 (log [nil 42 0.1 :keyword 'symbol "string" #"regexp" [1 2 3] {:k1 1 :k2 2} #{1 2 3}])                                         ; vector of simple values
 (log (range 100) (range 101) (range 220) (interleave (repeat :even) (repeat :odd)))                                           ; lists, including infinite ones
 (log {:k1 'v1 :k2 'v2 :k3 'v3 :k4 'v4 :k5 'v5 :k6 'v6 :k7 'v7 :k8 'v8 :k9 'v9})                                               ; maps
