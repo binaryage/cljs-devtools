7c55f47f44e3183b6cb740312fb054732ca5c00d
Ike Mawira <ike@superstruct.nz>
2021-12-21 09:05:17 +0300

    Revert - Push to history even when :render-path-annotations is false to enable circular object detection

--- src/lib/devtools/formatters/printing.cljs
+++ src/lib/devtools/formatters/printing.cljs
@@ -125,8 +125,7 @@
           circular? (is-circular? obj)
           inner-writer (make-template-writer (:markup-db opts))
           path-info (get-current-path-info)]
-      (when (pref :render-path-annotations)
-        (push-object-to-current-history! obj))
+      (push-object-to-current-history! obj)
       (alt-printer-job obj inner-writer opts)
       (.merge writer (post-process-printed-output (.get-group inner-writer) obj markup-db circular? path-info)))))
 
