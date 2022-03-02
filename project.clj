b1a28d351a4187018885a4e0bdef2a89ad5d8f1f
Antonin Hildebrand <antonin@hildebrand.cz>
2022-03-02 16:00:42 +0100

    version 1.0.5

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def clojurescript-version (or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.11.4"))
-(defproject binaryage/devtools "1.0.4"
+(defproject binaryage/devtools "1.0.5"
   :description "A collection of Chrome DevTools enhancements for ClojureScript developers."
   :url "https://github.com/binaryage/cljs-devtools"
   :license {:name         "MIT License"
